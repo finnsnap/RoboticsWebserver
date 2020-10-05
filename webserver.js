8caf87a494bda24efeb1728063ee5e8fc5acad17
finnsnap <1031061@gmail.com>
2020-10-05 22:24:02 +0100

    Working on mqtt

--- webserver.js
+++ webserver.js
@@ -24,4 +24,21 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
       console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
     }
   });
-}); 
+});
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+ 
+client.on('connect', function () {
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
