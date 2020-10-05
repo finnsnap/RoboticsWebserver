1438ae17cb806d19bb78f129f3f21a0b035c6870
finnsnap <1031061@gmail.com>
2020-10-05 15:13:20 -0500

    Working on webserver

--- webserver.js
+++ webserver.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http) //require socket.io module and pass the http
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+  fs.readFile(__dirname + '/public/mqqt-panelindex.html', function(err, data) { //read file index.html in public folder
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
       return res.end("404 Not Found");
